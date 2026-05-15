cee9a172c1df3ac4d35d0f6590eb8fb70cbb1796
kai <kai@gmail.com>
2026-05-15 19:46:47 +0300

    ייעע

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -1,12 +1,51 @@
-﻿namespace StudentBinding
+﻿using StudentBinding.Models;
+
+namespace StudentBinding
 {
     public partial class MainPage : ContentPage
     {
+        private Student displayedStudent;
+        private Student student1;
+        private Student student2;
+        public Student DisplayedStudent
+        {
+            get { return displayedStudent; }
+            set { displayedStudent = value; OnPropertyChanged(); }
+        }
         
         public MainPage()
         {
             InitializeComponent();
-        }
+           student1 = new Student
+            {
+                Name = "hampter",
+                ImageSource = "hampter.jpg",
+                BirthDate = new DateTime(2000, 1, 1)
+            };
+           student2 = new Student
+            {
+                Name = "mort",
+                ImageSource = "mort.jpg",
+                BirthDate = new DateTime(2001, 2, 2)
+            };
 
+            DisplayedStudent = student1;
+            this.BindingContext = this;
+
+        }
+       
+      private void StudentSwitchClick(object sender, EventArgs e)
+        {
+            if (DisplayedStudent== student1)
+            {
+                DisplayedStudent = student2;
+               
+            }
+            else
+            {
+                DisplayedStudent = student1;
+                
+            }
+        }
     }
 }
---
+++ StudentBinding/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentBinding.Models
+{
+    public class Student: ObservableObject
+
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); } 
+        }
+
+        private string imageSource;
+        public string ImageSource
+        {
+            get { return imageSource; }
+            set { imageSource = value; OnPropertyChanged(); }
+        }
+
+        private DateTime birthDate;
+        public DateTime BirthDate
+        {
+            get => birthDate;
+            set { birthDate = value; OnPropertyChanged(); }
+        }
+    }
+}
